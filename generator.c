ea58e3f754c4c1ea2becdec9a4e8c9517ccdae3b
Tommaso Ugolotti <tommasougolotti@gmail.com>
2018-12-23 06:24:15 -0800

    matteo non ha pazienza

--- generator.c
+++ generator.c
@@ -3,16 +3,15 @@
 #include <wait.h>
 #include <stdio.h>
 
-int main(){
+int main(int argc, char** argv){
     int POP_SIZE = 5;
     //const char sonFile[8] = "STUDENT";
-    char *argv[] = { NULL };
+    // char *argv[] = { NULL };
 
     for(int i=0;i<POP_SIZE;i++){ // loop will run n times 
      
         printf("\n POP_SIZE : %d" , i);
         if(fork() == 0){  
-            printf("\n fork");
             execve ("STUDENT", argv, NULL);
          } 
     } 
