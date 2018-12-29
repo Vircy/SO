6daab6bebb6b36b5070e1746a303e6fc9a72fbb1
Tommaso Ugolotti <tommasougolotti@gmail.com>
2018-12-29 11:35:35 -0800

    fixerino

--- generator.c
+++ generator.c
@@ -7,9 +7,9 @@
 #include <sys/sem.h>
 
 #define KEY (1492)
+#define POP_SIZE 5
 
-int main(int argc, char** argv){
-    unsigned int POP_SIZE = 5; 
+int seminit(){
     int sem;
     struct sembuf* semops = malloc(sizeof(struct sembuf)*1);
     semops->sem_num = 0;
@@ -18,10 +18,36 @@ int main(int argc, char** argv){
 
     sem = semget(KEY, 1, 0644 | IPC_EXCL | IPC_CREAT);
     semop(sem, semops, 1);
-    for(int i=0;i<POP_SIZE;i++){ // loop will run n times 
 
+    return sem;
+}
+
+struct sembuf* generateWaitOp(){
+    struct sembuf* semops = malloc(sizeof(struct sembuf)*2);
+
+    struct sembuf* decrementOp = semops;
+    decrementOp->sem_num = -1;
+    decrementOp->sem_op = POP_SIZE;
+    decrementOp->sem_flg = IPC_NOWAIT;
+
+    struct sembuf* waitOp = semops + 1;
+    waitOp->sem_num = 0;
+    waitOp->sem_op = POP_SIZE;
+    waitOp->sem_flg = IPC_NOWAIT;
+    
+    return semops;
+}
+
+
+int main(int argc, char** argv){
+    int sem = seminit();
+    struct sembuf* waitOp = generateWaitOp();
+    
+    for(int i=0;i<POP_SIZE;i++){ // loop will run n times 
+        
         printf("\n POP_SIZE : %d" , i);
-        if(fork() == 0){  
+        if(fork() == 0){
+            semop(sem, waitOp, 2);
             execve ("STUDENT", argv, NULL);
          } 
     } 
